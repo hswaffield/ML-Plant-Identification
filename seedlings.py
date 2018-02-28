10d98c2fab906e7af1ce1665ac30c76b8a940e82
Henry Swaffield <hhswaff@gmail.com>
2018-02-28 16:39:12 -0500

    typo correction

--- seedlings.py
+++ seedlings.py
@@ -102,15 +102,16 @@ def main():
 
     # dataaugmentation tools... lots of params to tweak.
     train_datagen = ImageDataGenerator(
-            rotation_range=40,
+            rotation_range=90,
             width_shift_range=0.2,
             height_shift_range=0.2,
             rescale=1./255,
             shear_range=0.2,
             zoom_range=0.2,
             horizontal_flip=True,
-            vertical__flip=True,
+            vertical_flip=True,
             fill_mode='nearest')
+    # new vertical flip, and 
 
     # train_datagen = ImageDataGenerator(
     #         rescale=1./255,
